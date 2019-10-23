21d1d3fd6e476e1a1144e5ff2fea00550a151d68
Christian Diener <ch.diener@gmail.com>
2019-10-23 11:05:11 -0700

    Several fixes and improvements.
    Improved the Article Pubmed parser you can no just pass any XML results
    list and it will parse this. Also fixed issues with titles being chopped
    off when they included HTML tags.
    
    Updated the articles.
    
    Compatibility fixes with newer hugo versions. The correct cache
    dir is now always used.
    
    Updated the info on MICOM.

--- content/packages/micom.md
+++ content/packages/micom.md
@@ -1,17 +1,18 @@
 +++
-repo = "https://github.com/resendislab/micom"
-title = "micom"
+repo = "https://github.com/micom-dev/micom"
+title = "MICOM"
 date = "2017-03-27T11:52:51-06:00"
 owner = "Christian Diener and Osbaldo Resendis-Antonio"
-website = "https://resendislab.github.io/micom"
+website = "https://micom-dev.github.io/micom"
 tags = ["microbiota", "microbiome", "community modeling"]
 +++
 
-micom is a Python package for metabolic modeling of microbial communities
-and is developed in the Human Systems Biology Group at the INMEGEN, Mexico.
+MICOM is a Python package for metabolic modeling of microbial communities
+and is developed in the Human Systems Biology Group at the INMEGEN, Mexico
+abd Institute for Systems Biology, Seattle, USA.
 
-micom allows you to construct a community model from a list on input COBRA
+MICOM allows you to construct a community model from a list on input COBRA
 models and manages exchange fluxes between individuals and individuals with the
 environment. It explicitly accounts for different abundances of individuals in
-the community and can thus incorporate data from 16S rRNA sequencing or metagenomic
+the community and can thus incorporate data from 16S amplicon sequencing or metagenomic
 shotgun experiments.
--- pubmed_to_hugo.py
+++ pubmed_to_hugo.py
@@ -19,6 +19,13 @@ def text_or_default(node, el, default):
     return res.text if res else default
 
 
+def content_str(el):
+    s = ["" if el.text is None else el.text] + [
+        ET.tostring(e, encoding="unicode") for e in el.getchildren()
+    ]
+    return "".join(s).replace('"', '\\"').replace("\n", "")
+
+
 if __name__ == "__main__":
     import xml.etree.ElementTree as ET
     from sys import argv
@@ -32,29 +39,39 @@ if __name__ == "__main__":
     for child in root:
         article = {}
         article["pmid"] = child.find(".//PMID").text
-        article["title"] = child.find(".//ArticleTitle").text
+        article["title"] = content_str(child.find(".//ArticleTitle"))
         if child.find(".//AbstractText") is not None:
             article["abstract"] = child.find(".//AbstractText").text
         else:
             article["abstract"] = ""
-        article["authors"] = ", ".join([
-            '"' + x.find("ForeName").text + " " + x.find("LastName").text + '"'
-            for x in child.findall(".//Author")
-            ])
-        article["keywords"] = ", ".join([
-            '"' + x.text + '"' for x in child.findall(".//Keyword")
-            ])
-        article["doi"] = child.find(
-            ".//ArticleId[@IdType='doi']").text
+        article["authors"] = ", ".join(
+            [
+                '"'
+                + x.find("ForeName").text
+                + " "
+                + x.find("LastName").text
+                + '"'
+                for x in child.findall(".//Author")
+            ]
+        )
+        article["keywords"] = ", ".join(
+            [
+                '"' + x.text.replace('"', '\\"').replace("\n", "") + '"'
+                for x in child.findall(".//Keyword")
+            ]
+        )
+        article["doi"] = child.find(".//ArticleId[@IdType='doi']").text
         article["journal"] = child.find(".//Journal/Title").text
         date = child.find(".//ArticleDate")
         if not date:
             date = child.find(".//PubMedPubDate[@PubStatus='pubmed']")
         month = text_or_default(date, "Month", "1")
         day = text_or_default(date, "Day", "1")
-        article["date"] = da(int(date.find("Year").text),
-                             int(date.find("Month").text),
-                             int(date.find("Day").text)).isoformat()
+        article["date"] = da(
+            int(date.find("Year").text),
+            int(date.find("Month").text),
+            int(date.find("Day").text),
+        ).isoformat()
         print(article["keywords"])
         filename = "content/pubs/PM{}.md".format(article["pmid"])
         with open(filename, "w") as f:
